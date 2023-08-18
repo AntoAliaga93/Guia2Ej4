c2cfc5e1f3321099d0293fc014480f872c057d67
AntoAliaga93 <anto.0aliaga@gmail.com>
2023-08-18 18:15:29 -0300

    Initial commit

---
+++ src/guia2ej4/Rubro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia2ej4;
+
+/**
+ *
+ * @author Anto
+ */
+public enum Rubro {
+    TODAS,
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA
+}
